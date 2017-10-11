45cc2b7dda9fb4b50407e8c3686619cf3bc753bf
kazi <kazi-Imran.Aziz@adviqo.com>
2017-10-11 15:02:14 +0200

    base entity added

---
+++ src/main/java/com/myecommerce/data/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.myecommerce.data;
+
+
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+ @CreatedDate
+ private Long createdAt; 
+
+ @LastModifiedDate
+ private Long lastModified;
+
+}
+
--- src/main/java/com/myecommerce/user/User.java
+++ src/main/java/com/myecommerce/user/User.java
@@ -1,5 +1,38 @@
 package com.myecommerce.user;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	@Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	 private Long id;
+
+	 private String firstName;
+
+	 private String lastName;
+
+	 private String email;
+
+//	 @OneToOne(cascade = CascadeType.ALL)
+//	 private Account account;
+
+	 public User(String firstName, String lastName, String email/*Account account
+			  */
+	  ) {
+	  this.firstName = firstName;
+	  this.lastName = lastName;
+	  this.email = email;
+	  //this.account = account;
+	 }
 }
